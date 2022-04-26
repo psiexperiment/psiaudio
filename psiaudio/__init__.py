3262ae7c56fd543ea1a3e5bc359a833be659ddc1
Brad Buran <bburan@alum.mit.edu>
2022-04-26 16:09:18 -0700

    Move add_logging_level to psiaudio

--- psiaudio/__init__.py
+++ psiaudio/__init__.py
@@ -1,1 +1,22 @@
+import logging
+
+
 __version__ = '2021.12.14.dev0'
+
+
+def add_logging_level(name, level):
+    logging.addLevelName(level, name)
+    # This step is required for coloredlogs to properly map the level name
+    setattr(logging, name, level)
+    def trace(self, message, *args, **kws):
+        nonlocal level
+        # Yes, logger takes its '*args' as 'args'.
+        if self.isEnabledFor(level):
+            self._log(level, message, args, **kws)
+    logging.Logger.trace = trace
+
+
+# Set up a verbose debugger level for tracing
+add_logging_level('TRACE', 5)
+log = logging.getLogger(__name__)
+log.addHandler(logging.NullHandler())
