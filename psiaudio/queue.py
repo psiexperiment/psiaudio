2cbdf5da70f31cb26ba46d3b09ead80a8b495d3b
Brad Buran <info@bradburan.com>
2023-04-07 16:15:33 -0700

    Fix formatting of long ine

--- psiaudio/queue.py
+++ psiaudio/queue.py
@@ -109,7 +109,8 @@ class AbstractSignalQueue:
         # Reset the samples
         new_sample = int(round((t - self._t0) * self._fs))
         if check and (new_sample > self._samples):
-            raise ValueError(f'Cannot rewind past last sample generated. Requested {t:.3f}s, last sample was {self.get_ts():.3f}s.')
+            raise ValueError(f'Cannot rewind past last sample generated. ' \
+                             'Requested {t:.3f}s, last sample was {self.get_ts():.3f}s.')
         self._samples = new_sample
         log.debug('Rewound queue samples back to %d', self._samples)
 
