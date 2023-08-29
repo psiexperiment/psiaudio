Correct derivative for sampling rate
This ensures derivative is in units of /sec

psiaudio/pipeline.py
@@ -1149,7 +1149,7 @@ def derivative(initial_state, target):
                                      metadata=new_samples.metadata)
     while True:
         samples = concat((initial_state, new_samples), axis=-1)
-        target(np.diff(samples))
+        target(np.diff(samples) * samples.fs)
         initial_state = samples[..., -1:]
         new_samples = (yield)
 

tests/test_pipeline.py
@@ -864,7 +864,7 @@ def test_derivative(fs, data_fixture, request):
     pad_shape = ((0, 0), (1, 0)) if data_fixture == 'data2d' else (1, 0)
     padded_data = np.pad(data, pad_shape, 'constant', constant_values=0)
     expected = pipeline.PipelineData(
-        np.diff(padded_data),
+        np.diff(padded_data) / data.fs,
         s0=1,
         fs=data.fs,
         channel=data.channel,