b621cb4e144923e4250cdd36fd3e7d92759aecc4
Brad Buran <buran@ohsu.edu>
2023-10-18 14:47:52 -0700

    Update weighting function to allow null weight

--- psiaudio/weighting.py
+++ psiaudio/weighting.py
@@ -27,9 +27,21 @@ def load(freq, name):
     ----------
     freq : array
         Frequencies to include weights for
-    name : {'mouse'}
-        Weights to load
+    name : {'mouse', None, np.nan}
+        Weights to load. If None or np.nan, then weights will be set to a null
+        operation (0 dB) across all frequencies.
+
+    Returns
+    -------
+    weights : array
+        Weights for eacy frequency in `freq` in dB.
     '''
+    # Short-circut if no weights desired (this is useful in cases where we want
+    # to have simple code that can easily switch between weighted and
+    # unweighted without having to add an if-statement in the calling code. Use
+    # str(nan) to check value since np.isnan does not work on strings.
+    if str(name) == 'nan' or name is None:
+        return np.zeros_like(freq)
     weights = pd.Series(WEIGHTS[name])
     w_freq = weights.index.values
     w_level = weights.values
