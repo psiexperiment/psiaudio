4b29849562443ab86fbdc5ac05743df8e4875a17
Brad Buran <buran@ohsu.edu>
2026-04-20 14:17:16 -0700

    Fix deprecation in numpy

--- examples/gallery/bandlimited-click.py
+++ examples/gallery/bandlimited-click.py
@@ -38,7 +38,7 @@ for flb, fub in [(2e3, 32e3), (8e3, 32e3)]:
         # would get a calibration error otherwise.
         spl = cal.get_db(psd.iloc[1:])
         psd_spl = util.rms_rfft_db(spl)
-        pe_spl = cal.get_db(1e3, waveform.ptp())
+        pe_spl = cal.get_db(1e3, np.ptp(waveform))
         print(f'{level} {pe_spl} {psd_spl}')
 
         axes[0].plot(t * 1e3, waveform)
